0b9e24ba5432a83eaebd0179a5e8a5a92ce3af00
akshitsaini111 <sainiakshit85@gmail.com>
2023-09-20 23:12:30 +0530

    static data added

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.scss']
+  styleUrls: ['./recipes-list.component.scss'],
 })
 export class RecipesListComponent {
-
+  recipes: Recipes[] = [
+    {
+      name: 'Burgir',
+      description: 'Not Healthy',
+      imagePath:
+        'https://blog.dineout-cdn.co.in/blog/wp-content/uploads/2017/01/Blog-Banner-1400x400px-1024x293.jpg',
+    },
+    {
+      name: 'Burgir',
+      description: 'Not Healthy',
+      imagePath:
+        'https://blog.dineout-cdn.co.in/blog/wp-content/uploads/2017/01/Blog-Banner-1400x400px-1024x293.jpg',
+    },
+    {
+      name: 'Burgir',
+      description: 'Not Healthy',
+      imagePath:
+        'https://blog.dineout-cdn.co.in/blog/wp-content/uploads/2017/01/Blog-Banner-1400x400px-1024x293.jpg',
+    },
+  ];
 }
---
+++ src/app/recipes/recipes.ts
@@ -0,0 +1,5 @@
+export interface Recipes {
+    name: string
+    description: string,
+    imagePath:string
+}
