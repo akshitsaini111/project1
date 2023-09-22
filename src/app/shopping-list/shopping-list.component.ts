a40481ed6475dfdc225be88d0762eab4c5852d62
akshitsaini111 <sainiakshit85@gmail.com>
2023-09-23 05:04:19 +0530

    basic laoyout done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/ingredients';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredients[] = [
+    {
+      name: 'Bun',
+      amount: 10,
+    },
+    {
+      name: 'Bun',
+      amount: 10,
+    },
+    {
+      name: 'Bun',
+      amount: 10,
+    },
+  ];
 }
